069e354b9075eed1156a517bf96c80f63f18519b
NoahTheMeh <58647582+NoahTheMeh@users.noreply.github.com>
2024-03-11 19:50:12 -0600

    initial release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+// App.jsx
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import QuestGraph from './QuestGraph';
+import JsonInputPopup from './JsonInputPopup';
 
 function App() {
+  const [jsonText, setJsonText] = useState('');
+  const [showLabels, setShowLabels] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [parsedJsonText, setParsedJsonText] = useState({});
+
+  useEffect(() => {
+    document.title = "OSRS Quest Explorer";
+  }, []);
+
+  const handleJsonTextChange = (newJsonText) => {
+    setJsonText(newJsonText);
+    try {
+      const parsedJson = JSON.parse(newJsonText);
+      setParsedJsonText(parsedJson);
+    } catch (error) {
+    }
+  };
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const handlePopupClose = (event) => {
+    if (event.target.classList.contains('popup')) {
+      setIsPopupOpen(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="graph-container">
+        <div className="top-left-controls">
+          <div>
+            <label>
+              <input type="checkbox" checked={showLabels} onChange={() => setShowLabels(!showLabels)} />
+              Show Labels
+            </label>
+          </div>
+          <div>
+            <button onClick={togglePopup}>Load My Quest Data</button>
+          </div>
+        </div>
+        <div className="QuestGraph">
+          <QuestGraph WikiSync={parsedJsonText} showLabels={showLabels} />
+        </div>
+      </div>
+      {isPopupOpen && (
+        <div className="popup" onClick={handlePopupClose}>
+          <JsonInputPopup
+            jsonText={jsonText}
+            onClose={togglePopup}
+            onChange={handleJsonTextChange}
+          />
+        </div>
+      )}
     </div>
   );
 }
